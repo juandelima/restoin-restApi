955bf67dc209b9727eec686a200ad2349cceb6f1
Juan Valerian Delima <juanvaleriandelima9@gmail.com>
2020-07-20 00:30:28 +0700

    restoin restAPI v.1

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,6 +2,7 @@ module.exports = app => {
     const user = require('../controllers/user.controller.js');
     app.post("/user", user.create);
     app.get("/user", user.findAll);
+    app.put("/user/:id_user", user.update);
     app.delete("/user/:id_user", user.delete);
     app.delete("/user", user.deleteAll);
 }
